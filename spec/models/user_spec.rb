591968b5f3ceba5a547afc0df11355b16aca0094
Angel Cordova <ae_cordova@outlook.com>
2023-01-18 17:53:07 -0600

    change validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,53 +1,33 @@
 require 'rails_helper'
 
-
 RSpec.describe User, type: :model do
-  context "with valid data" do
-    let(:guest_user) { build(:guest_user) }
-    let(:registered_user) { build(:registered_user) }
-    let(:admin_user) { build(:admin_user) }
-
-    it "should be valid" do
-      expect(guest_user).to be_valid
-      expect(registered_user).to be_valid
-      expect(admin_user).to be_valid
+  describe 'validations' do
+    describe "guest user" do
+      let(:user) {create :guest_user }
+  
+      it { user.should validate_presence_of(:name) }
+      it { user.should_not validate_presence_of(:email) }
+      it { user.should_not validate_presence_of(:password) }
     end
-  end
 
-  describe "guest_user" do
-    it "just requires a name" do
-      guest_user = User.new(name: "Guest")
-      expect(guest_user).to be_valid
+    describe "admin user" do
+      let(:user) { build :admin_user }
+  
+      it { user.should validate_presence_of(:name) }
+      it { user.should validate_presence_of(:email) }
+      it { user.should validate_presence_of(:password) }
+      it { user.should_not allow_value("bleep").for(:email) } 
+      it { user.should allow_value("user@mail.com").for(:email) } 
     end
-  end
 
-  describe "admin_user" do
-    it "requires name, valid email, password and password_confirmation" do
-      admin_user = User.new(name: "Admin", role: "admin")
-      expected_errors = [
-        "Email can't be blank", 
-        "Email is invalid", 
-        "Password can't be blank", 
-        "Password is too short (minimum is 6 characters)"
-      ]
-
-      expect(admin_user.valid?).to be false
-      expect(admin_user.errors.full_messages).to match(expected_errors)
-    end
-  end
-
-  describe "registered_user" do
-    it "requires name, valid email, password and password_confirmation" do
-      admin_user = User.new(name: "User", role: "user")
-      expected_errors = [
-        "Email can't be blank", 
-        "Email is invalid", 
-        "Password can't be blank", 
-        "Password is too short (minimum is 6 characters)"
-      ]
-
-      expect(admin_user.valid?).to be false
-      expect(admin_user.errors.full_messages).to match(expected_errors)
+    describe "normal/registered user" do
+      let(:user) { build :registered_user }
+  
+      it { user.should validate_presence_of(:name) }
+      it { user.should validate_presence_of(:email) }
+      it { user.should validate_presence_of(:password) }
+      it { user.should_not allow_value("bleep").for(:email) } 
+      it { user.should allow_value("user@mail.com").for(:email) } 
     end
   end
 end
