a568e2b76abb880a7567dc04ab4361962b5e93a1
Angel Cordova <ae_cordova@outlook.com>
2023-01-23 13:49:42 -0600

    modify seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,29 @@
 require 'faker'
 require 'factory_bot_rails'
 
+QUESTION_LIST =  [
+  "How often do you feel comfortable in large groups of people?",
+  "How often do you prefer to spend time alone?",
+  "How often do you feel drained after social interactions?",
+  "How often do you enjoy being the center of attention?",
+  "How often do you feel energized by being around people?",
+  "How often do you prefer small gatherings over large parties?",
+  "How often do you find it easy to start a conversation with a stranger?",
+  "How often do you enjoy being alone?",
+  "How often do you feel comfortable in unfamiliar social situations?",
+  "How often do you need time alone to recharge after spending time with people?",
+]
+
+REVERSED_QUESTIONS = [2, 3, 6, 8, 10]
+
+CHOICE_LIST = [
+  'Never',
+  'Rarely',
+  'Sometimes',
+  'Often',
+  'Always',
+]
+ 
 def find_or_create_admin_user
   User.find_by(email: 'angel@email.com') ||
   FactoryBot.create(
@@ -18,6 +41,29 @@ def find_or_create_admin_user
 end
 
 admin_user = find_or_create_admin_user
-test = FactoryBot.create(:draft_test, author: admin_user)
-FactoryBot.create_list(:question, 10, :multiple_choice, test: test)
-puts "  -> Done!! Admin User and Test created with 10 questions"
+puts "\n- Admin User created: #{admin_user.email}"
+
+test = FactoryBot.create(:published_test, 
+  author: admin_user, 
+  calculation_type: :extrovertion_table_calculation,
+  title: 'Quick Personality Test'
+)
+puts "\n- Test created: #{test.title}, status: #{test.status}, calculation_class: #{test.calculation_class}"
+puts "\n- Creating 10 questions..."
+QUESTION_LIST.each_with_index do |question_caption, question_index|
+  question = FactoryBot.create(:question, :multiple_choice, test: test,  title: nil, caption: question_caption,  settings: { choices: [] })
+  puts "\n -> Question created: #{question_index + 1} - #{question_caption}"
+
+  choice_list =  REVERSED_QUESTIONS.include?(question_index + 1) ? CHOICE_LIST.reverse : CHOICE_LIST
+
+  question_choices = choice_list.each_with_index.with_object([]) do |(caption, i), choices|
+    choices << { order: i, caption: caption, value: i+1 }
+  end
+  
+  question_choices.reverse! if REVERSED_QUESTIONS.include?(question_index + 1)
+  
+  question.update(choices: question_choices)
+  puts "  -> Choices created: #{question_choices.map{|c| "#{c[:caption]}: #{c[:value]}"}.join(", ") }"
+end
+
+puts "\n- Done!! Admin User and Test created with 10 questions"
